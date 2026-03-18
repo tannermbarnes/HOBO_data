e6c2bc8dd3f875d61b9ad0e67a14b92c484b1938
Tanner Barnes <tannermbarnes@rover-239-32.rovernet.mtu.edu>
2026-03-18 13:16:33 -0400

    need HOBO data from winter 2026

--- code/mead_May2025-Sept2025.R
+++ code/mead_May2025-Sept2025.R
@@ -6,67 +6,67 @@ library(lubridate)
 
 entrance <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                        sheet = "gate_entrance") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Gate Entrance") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
 psy_entrance <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                       sheet = "psychrometer_entrance") %>% 
-  select(date, probe_temp, integrated_temp) %>%
+  select(datetime, probe, integrated) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Entrance Psychrometer") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59")) %>% 
-  mutate(temp = integrated_temp)
+  mutate(temp = integrated)
 
 wall_entrance <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                             sheet = "wall_entrance") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Wall Entrance") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
 middle_ICU <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                          sheet = "middle") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Middle ICU") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
 wall_back <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                             sheet = "wall_back") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Wall Back") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
 ICU_PSY <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                             sheet = "ICU_PSY") %>% 
-  select(date, probe_temp, integrated_temp) %>%
+  select(datetime, probe, integrated) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "ICU Psychrometer") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59")) %>% 
-  mutate(temp = integrated_temp)
+  mutate(temp = integrated)
 
 ICU_back <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                             sheet = "ICU_back") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "Back of ICU") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
 control <- read_excel("data/Mead/MEAD_DATA_MAY_2025_ON.xlsx", 
                       sheet = "control_200") %>% 
-  select(date, temp) %>%
+  select(datetime, temp) %>%
   mutate(
-    date_time = as.POSIXct(date, format ="Y-%m-%d %H:%M:%S"), 
+    date_time = as.POSIXct(datetime, format ="Y-%m-%d %H:%M:%S"), 
     source = "control") %>% 
   filter(date_time >= as.POSIXct("2024-10-01 11:32:59"))
 
@@ -196,25 +196,59 @@ ggsave("figures/mead_wallback_zoom.png",
        temp_plot_wall_back,
        width = 8, height = 4, dpi = 300)
 
-# ---- New Plot: Experiment Year 1 (Dec 2025 - Current) ----
+# ---- New Plot: Experiment Year 1 (Dec 2025 - Current) ---- ##########################
 experiment_year1_df <- bind_rows(
   psy_entrance,
+  wall_entrance,
   wall_back,
   middle_ICU,
-  ICU_back
+  ICU_back, 
+  control
 ) %>%
+  arrange(source, date_time) %>%
   mutate(source = case_when(
     source == "Entrance Psychrometer" ~ "External Temperature",
+    source == "Wall Entrance" ~ "Entrance",
     source == "Wall Back" ~ "Experiment - Front",
     source == "Middle ICU" ~ "Experiment - Middle",
     source == "Back of ICU" ~ "Experiment - Back",
+    source == "control" ~ "Control",
     TRUE ~ source
-  ))
+  )) %>%
+  mutate(source = factor(
+    source,
+    levels = c(
+      "External Temperature",
+      "Entrance",
+      "Experiment - Front",
+      "Experiment - Middle",
+      "Experiment - Back",
+      "Control"
+    )
+  )) %>%
+  group_by(source) %>%
+  mutate(
+    time_gap_days = as.numeric(difftime(date_time, lag(date_time), units = "days")),
+    line_group = cumsum(replace_na(time_gap_days > 7, FALSE))
+  ) %>%
+  ungroup()
 
 # Compute y-axis breaks for this plot
 y_breaks_year1 <- seq(-10, 10, by = 1)
 
-experiment_year1_plot <- ggplot(experiment_year1_df, aes(x = date_time, y = temp, color = source)) +
+experiment_year1_colors <- c(
+  "External Temperature" = "#1F78B4",
+  "Entrance" = "#E66100",
+  "Experiment - Front" = "#4DAF4A",
+  "Experiment - Middle" = "#984EA3",
+  "Experiment - Back" = "#A65628", 
+  "Control" = "#DAA520"
+)
+
+experiment_year1_plot <- ggplot(
+  experiment_year1_df,
+  aes(x = date_time, y = temp, color = source, group = interaction(source, line_group))
+) +
   geom_rect(data = data.frame(xmin = as.POSIXct("2025-12-11"), xmax = as.POSIXct("2025-12-16"), ymin = -Inf, ymax = Inf), aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), fill = "dodgerblue", alpha = 0.3, inherit.aes = FALSE) +
   geom_rect(data = data.frame(xmin = as.POSIXct("2026-02-17"), xmax = Sys.time(), ymin = -Inf, ymax = Inf), aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), fill = "dodgerblue", alpha = 0.3, inherit.aes = FALSE) +
   geom_line() +
@@ -231,6 +265,7 @@ experiment_year1_plot <- ggplot(experiment_year1_df, aes(x = date_time, y = temp
     date_labels = "%b %d",
     date_breaks = "2 weeks"
   ) +
+  scale_color_manual(values = experiment_year1_colors) +
   scale_y_continuous(limits = c(-10, 10), breaks = y_breaks_year1) +
   theme_minimal(base_size = 18) +
   theme(
@@ -241,7 +276,11 @@ experiment_year1_plot <- ggplot(experiment_year1_df, aes(x = date_time, y = temp
     legend.position = c(0.85, 0.20),
     legend.background = element_rect(fill = "white", color = "black")
   ) +
-  geom_hline(yintercept = 3, color = "red", linetype = "dashed")
+  geom_hline(yintercept = 3, color = "red", linetype = "dashed") +
+  geom_vline(xintercept = as.POSIXct("2025-12-11"), color = "darkorange", linetype = "solid") +
+  geom_vline(xintercept = as.POSIXct("2026-02-27"), color = "darkblue", linetype = "solid") +
+  annotate("text", x = as.POSIXct("2025-12-14"), y = 9, label = "Survey 1", color = "darkorange", size = 4, angle = 90, vjust = -0.5) +
+  annotate("text", x = as.POSIXct("2026-02-27"), y = 9, label = "Survey 2", color = "darkblue", size = 4, angle = 90, vjust = -0.5)
 
 print(experiment_year1_plot)
 
